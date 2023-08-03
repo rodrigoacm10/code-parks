ea6ef34a5b1088bc5b3a4841c1dee326f5e8652e
rodrigoacm10 <rodrigoacm10@gmail.com>
2023-08-03 09:02:42 -0300

    bidimensional-array

---
+++ arrayBidimensionalCodePark18/script.js
@@ -0,0 +1,15 @@
+// Nesta atividade, você deve criar um código em JavaScript com uma matriz bidimensional para armazenar informações sobre animais e as suas características.
+
+// Cada linha deve representar um animal e cada coluna terá as diferentes informações sobre ele, como nome, espécie e idade.
+
+console.log("ass");
+
+const animals = [
+  ["Leão", "Felino", 5],
+  ["Golfinho", "Mamífero Aquático", 10],
+  ["Águia", "Ave de Rapina", 8],
+  ["Tartaruga", "Réptil", 50],
+  ["Girafa", "Mamífero", 7],
+];
+
+console.log(animals);
