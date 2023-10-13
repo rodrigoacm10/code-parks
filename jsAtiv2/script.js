760501df07683a571a77db703168811440e2d35b
rodrigoacm10 <rodrigoacm10@gmail.com>
2023-10-13 17:55:09 -0300

    calc-media-notas

---
+++ jsAtiv2/script.js
@@ -0,0 +1,17 @@
+const calcMedia = function () {
+  const yesOrNo = prompt("Deseja saber a média de três notas? (s/n)");
+
+  if (yesOrNo == "n") {
+    return;
+  } else if (yesOrNo != "s") {
+    alert("insira um valor correto");
+    return;
+  }
+
+  const nota1 = +prompt("insira a primeira nota (0 - 10):");
+  const nota2 = +prompt("insira a segunda nota (0 - 10):");
+  const nota3 = +prompt("insira a terceira nota (0 - 10):");
+  const media = [nota1, nota2, nota3].reduce((acc, num) => (acc += num), 0) / 3;
+  console.log(media);
+};
+calcMedia();
